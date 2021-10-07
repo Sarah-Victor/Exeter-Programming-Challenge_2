41d35ade60a6658c95752ae993d24795087c0c35
Sarah Philomin V <sarahphilomin@gmail.com>
2021-10-07 12:40:16 +0530

    Delete Items.js

--- Items.js
+++
@@ -1,8 +0,0 @@
-let Student = [
-    {id: 'S1', name: 'Student1', subject1: '100',subject2: '98',subject3: '76',subject4:'82',subject5:'92'},
-    {id: 'S2', name: 'Student2', subject1: '92',subject2: '100',subject3: '98',subject4:'76',subject5:'82'},
-    {id: 'S3', name: 'Student3', subject1: '76',subject2: '92',subject3: '100',subject4:'98',subject5:'76'},
-    {id: 'S4', name: 'Student4', subject1: '82',subject2: '76',subject3: '92',subject4:'100',subject5:'98'},
-]
-
-module.exports = items
